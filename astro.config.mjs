dfd8f1f0050011c65a6ebf8eee256d9a99c70cf4
Taurean Bryant <later@taurean.work>
2025-02-25 12:11:57 -0800

    adding some vercel adapter stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://taurean.work",
+    output: "server",
+    adapter: vercel(),
+});
