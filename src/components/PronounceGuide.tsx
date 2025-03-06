eb7a36809b817dc0591477f14c57f595b8a059d4
Taurean Bryant <later@taurean.work>
2025-03-05 23:45:34 -0800

    Make pronunciation mp3 load on component mount (#2)
    * shifting code around so audio is loaded on mount

--- src/components/PronounceGuide.tsx
+++ src/components/PronounceGuide.tsx
@@ -1,34 +1,35 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export const PronounceGuide = () => {
-    const [playing, setIsPlaying] = useState<boolean>(false);
-    const audioRef = useRef<HTMLAudioElement | null>(null);
-    const mp3Path =
-        "https://cdn.taurean.work/name%20pronounciation%20guide.mp3";
+  const [playing, setIsPlaying] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const mp3Path = "https://cdn.taurean.work/name%20pronounciation%20guide.mp3";
 
-    const handleOnClick = () => {
-        // Create audio object on first click (lazy initialization)
-        if (!audioRef.current) {
-            audioRef.current = new Audio(mp3Path);
-            audioRef.current.onended = () => setIsPlaying(false);
-        }
+  // Create audio object on component mount
+  useEffect(() => {
+    audioRef.current = new Audio(mp3Path);
+    audioRef.current.onended = () => setIsPlaying(false);
+  }, []);
 
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
+  const handleOnClick = () => {
+    if (!audioRef.current) return;
 
-        audioRef.current
-            .play()
-            .then(() => setIsPlaying(true))
-            .catch((error) => console.error("Error playing audio:", error));
-    };
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
 
-    return (
-        <button
-            onClick={handleOnClick}
-            className={`button ${playing ? "isPlaying" : ""}`}
-        >
-            <span data-visually-hidden>Pronounce</span>
-            <span>tor-ee-ehn</span>
-        </button>
-    );
+    audioRef.current
+      .play()
+      .then(() => setIsPlaying(true))
+      .catch((error) => console.error("Error playing audio:", error));
+  };
+
+  return (
+    <button
+      onClick={handleOnClick}
+      className={`button ${playing ? "isPlaying" : ""}`}
+    >
+      <span data-visually-hidden>Pronounce</span>
+      <span>tor-ee-ehn</span>
+    </button>
+  );
 };
