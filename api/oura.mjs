e5f0fbe9559f9182a6fc147a15849b49224c1727
Taurean Bryant <later@taurean.work>
2025-03-07 00:39:02 -0800

    fixing env var stuff… (#5)

--- api/oura.mjs
+++ api/oura.mjs
@@ -1,12 +1,10 @@
-import { waitUntil, getEnv } from "@vercel/functions";
 import { createClient } from "@supabase/supabase-js";
 
-const {
-  SUPABASE_URL,
-  SUPABASE_SERVICE_ROLE_KEY,
-  OURA_API_KEY,
-  OURA_TABLE_NAME,
-} = getEnv();
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const OURA_API_KEY = process.env.OURA_API_KEY;
+const OURA_TABLE_NAME = process.env.OURA_TABLE_NAME;
+
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
